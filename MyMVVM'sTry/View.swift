e80c084ff346025614378cce687f28b8ac05d997
ColdSun <coldsun1395@gmail.com>
2024-07-09 18:44:16 +0300

    the first commit

---
+++ MyMVVM'sTry/View.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMVVM'sTry
+//
+//  Created by Игорь Тимофеев on 9.07.24.
+//
+
+import UIKit
+
+class View: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
